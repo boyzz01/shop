751c5e895a2e1f7a59c461f44d52d90d430248a2
Ardi Sahputra <ardysyahputra01@gmail.com>
2022-11-30 17:45:10 +0700

    Update super.blade.php

--- resources/views/partials/admin-role/super.blade.php
+++ resources/views/partials/admin-role/super.blade.php
@@ -352,7 +352,7 @@
             </li>
         </ul>
     </li>
-
+    {{-- 
     <li>
         <a href="#emails" class="accordion-toggle wave-effect" data-toggle="collapse" aria-expanded="false">
             <i class="fas fa-at"></i>{{ __('Email Settings') }}
@@ -362,7 +362,7 @@
             <li><a href="{{ route('admin-mail-config') }}"><span>{{ __('Email Configurations') }}</span></a></li>
             <li><a href="{{ route('admin-group-show') }}"><span>{{ __('Group Email') }}</span></a></li>
         </ul>
-    </li>
+    </li> --}}
 
     {{-- <li>
         <a href="#payments" class="accordion-toggle wave-effect" data-toggle="collapse" aria-expanded="false">
